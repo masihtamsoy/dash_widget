491935ca8d0bcd70a4bd1487b05f7d6ebea2f90a
masihtamsoy <masihtamsoy@gmail.com>
2022-01-18 11:29:01 +0530

    Added counter timer

--- lib/dash_widget.dart
+++ lib/dash_widget.dart
@@ -4,5 +4,4 @@ export './src/button_widget.dart';
 export './src/job_card_widget.dart';
 export 'wrapper/jobs_store_widget_wrapper.dart';
 export 'wrapper/listing_ui_store_wizard.dart';
-export 'wrapper/quiz_form_builder.dart';
 export 'wrapper/quiz_forms_countdown_wizard.dart';
--- lib/src/countdown_timer_widget.dart
+++ lib/src/countdown_timer_widget.dart
@@ -37,20 +37,30 @@ class _CountdownTimerWidgetState extends State<CountdownTimerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Countdown(
-            countdownController: countdownController,
-            builder: (_, Duration time) {
-              return Text(
-                  'minutes: ${time.inMinutes % 60} seconds: ${time.inSeconds % 60}');
-              // return Text(
-              //   'hours--: ${time.inHours} minutes: ${time.inMinutes % 60} seconds: ${time.inSeconds % 60}',
-              //   style: TextStyle(fontSize: 20),
-              // );
-            }),
-      ),
-    );
+    return Countdown(
+        countdownController: countdownController,
+        builder: (_, Duration time) {
+          return Text(
+              'minutes: ${time.inMinutes % 60} seconds: ${time.inSeconds % 60}');
+          // return Text(
+          //   'hours--: ${time.inHours} minutes: ${time.inMinutes % 60} seconds: ${time.inSeconds % 60}',
+          //   style: TextStyle(fontSize: 20),
+          // );
+        });
+    // return Scaffold(
+    //   body: Center(
+    //     child: Countdown(
+    //         countdownController: countdownController,
+    //         builder: (_, Duration time) {
+    //           return Text(
+    //               'minutes: ${time.inMinutes % 60} seconds: ${time.inSeconds % 60}');
+    //           // return Text(
+    //           //   'hours--: ${time.inHours} minutes: ${time.inMinutes % 60} seconds: ${time.inSeconds % 60}',
+    //           //   style: TextStyle(fontSize: 20),
+    //           // );
+    //         }),
+    //   ),
+    // );
   }
 
   @override
